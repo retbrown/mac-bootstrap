e2cc3bad65fda60daa8b80efc3d1dd324ca1e17e
Robert Brown <robert@slan124.info>
2019-03-24 10:58:07 +0000

    Initial version with config and packages

---
+++ script.sh
@@ -0,0 +1,8 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#one install complete check command works
+#install line homebrew packages
+#install cask packages
+
+#copy mackup config
+#copy shuttle config to get started
