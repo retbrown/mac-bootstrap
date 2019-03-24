0e782b9c88cba7699ebbf264313a0dc48508a5d2
Robert Brown <robert@slan124.info>
2019-03-24 18:25:55 +0000

    Version that should work

--- script.sh
+++ script.sh
@@ -1,8 +1,27 @@
+#!/bin/bash
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #one install complete check command works
+brew commands
+if [ $? -eq 0 ]; then
+    echo "Brew OK"
+else
+    exit 1
+fi
 #install line homebrew packages
+BREW_PACKAGES=$(curl -fsSL https://raw.githubusercontent.com/retbrown/mac-bootstrap/master/homebrew)
+
+echo "$BREW_PACKAGES"
+
+brew install $BREW_PACKAGES
+
 #install cask packages
+CASK_PACKAGES=$(curl -fsSL https://raw.githubusercontent.com/retbrown/mac-bootstrap/master/homebrew-cask)
+echo "$CASK_PACKAGES"
+brew cask install $BREW_PACKAGES
 
 #copy mackup config
+curl -fsSL https://raw.githubusercontent.com/retbrown/mac-bootstrap/master/.mackup.cfg -o ~/.mackup.cfg
 #copy shuttle config to get started
+curl -fsSL https://raw.githubusercontent.com/retbrown/mac-bootstrap/master/.shuttle.json -o ~/.shuttle.json
